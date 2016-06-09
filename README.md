ec3c89ca50ac35ad40e6cab0578736e512c14d4c
Zarko Selak <zarko.selak@trikoder.net>
2016-06-10 01:58:37 +0200

    Tests & readme update
    - adds mocha cai tests
    - updates readme

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# object-extend-js :raised_hands:
+# object-update-js :raised_hands:
+
+Updates <b>ONLY<b> existing properties!!
+
+## Example
+
+```javascript
+var updateObj = require('object-update-js');
+
+var obj1 = {
+	t: 'test1',
+	te: {
+		t: tt,
+		e: ee
+	},
+	ff: ['tt', 'ee', 'te']
+};
+
+var obj2 = {
+	t: 'test2',
+	te: {
+		t: xx,
+		e: yy
+	},
+	ff: ['xx', 'yy', 'zz'],
+	qq: 'omg!'
+};
+
+var result = updateObj(obj1, obj2);
+result -->
+{
+	t: 'test2',
+	te: {
+		t: xx,
+		e: yy
+	},
+	ff: ['xx', 'yy', 'zz']
+}
+```
+
+## Test
+
+`npm test`
+
+## License
+
+MIT
+
--- test.js
+++ test.js
@@ -1,3 +1,45 @@
 var extend = require('./index.js');
+var expect = require('chai').expect;
 
-//TODO: Add test
+describe('ObjectExtend', function() {
+	it('should update object attribute', function() {
+		var result = extend({troll: 'face'}, {troll: 'loloo'});
+
+		expect(result).to.deep.equal({troll: 'loloo'});
+	});
+
+	it('should not add new property', function() {
+		var result = extend({troll: 'face'}, {rofl: 'copter'});
+
+		expect(result).to.not.have.property('rofl');
+	});
+
+	it('should update nested properties', function() {
+		var troll = {
+			face: 'trollface',
+			color: {
+				red: true
+			}
+		};
+
+		var roll = {
+			face: 'roll',
+			color: {
+				red: false
+			}
+		};
+		var result = extend(troll, roll);
+
+		expect(result).to.have.property('color')
+			.that.deep.equals({red: false});
+	});
+
+	it('should return object', function() {
+		var troll = {troll: 'face'};
+		var roll  = {roll: 'mace'};
+
+		var result = extend(troll, roll);
+
+		expect(result).to.be.an('object');
+	});
+});
